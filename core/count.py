23d7593f543c51d5a59ed78ee19259d07e7b8a81
nishantr05 <66346272+nishantr05@users.noreply.github.com>
2021-01-23 09:16:11 +0530

    Update count.py

--- core/count.py
+++ core/count.py
@@ -3,22 +3,21 @@ import cv2
 import random
 import numpy as np
 import tensorflow as tf
-import pytesseract
 from core.utils import read_class_names
 from core.config import cfg
 
 # function to count objects, can return total classes or count per class
 def count_persons(data):#, by_class = False, allowed_classes = list(read_class_names(cfg.YOLO.CLASSES).values())):
 
-    boxes, scores, classes, num_objects = data
-
+    #boxes, scores, classes, num_objects = data
+    classes, num_objects = data[2], data[3]
     #create dictionary to hold count of objects
     count_val = 0
 
     # if by_class = True then count objects per class
     #if by_class:
     #    class_names = read_class_names(cfg.YOLO.CLASSES)
-
+    class_names = read_class_names(cfg.YOLO.CLASSES)
     # loop through total number of objects found
     for i in range(num_objects):
         # grab class index and convert into corresponding class name
